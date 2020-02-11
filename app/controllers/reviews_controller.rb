b2f43f65b156c6da260804585f76bfd78f27025d
Miles Bader <milesdbader@gmail.com>
2020-02-11 15:37:01 +0200

    finished w section that Kitt calls "Routing / Controllers"

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+  before_action :load_restaurant
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def load_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+puts 'clearing database'
+Restaurant.destroy_all
+puts "Databate empty; #{Restaurant.count} elements"
+puts '...'
 puts "Generating restaurants"
 
 rand(8..15).times do
   Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
-    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.phone_number
   )
 end
 
